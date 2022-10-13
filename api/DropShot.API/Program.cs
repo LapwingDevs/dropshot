00d6f8fdd1d318e4b0978fb3b16da6a79351db96
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-10-13 20:33:06 +0200

    init web app project

--- api/DropShot.API/Program.cs
+++ api/DropShot.API/Program.cs
@@ -1,20 +1,33 @@
+const string defaultCorsPolicy = "CorsPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var origins = builder.Configuration["origins"].Split(";");
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(defaultCorsPolicy,
+        corsPolicyBuilder =>
+        {
+            corsPolicyBuilder
+                .WithOrigins(origins)
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(defaultCorsPolicy);
 
 app.UseHttpsRedirection();
 
