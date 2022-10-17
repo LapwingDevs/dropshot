090a4771e97d6813571c465a563dd861e8a7915f
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-10-17 20:38:26 +0200

    db context

---
+++ api/DropShot.Infrastructure/DAL/DropShotDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DropShot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DropShot.Infrastructure.DAL;
+
+public class DropShotDbContext : DbContext
+{
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Drop> Drops { get; set; }
+    public DbSet<DropItem> DropItems { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Variant> Variants { get; set; }
+    
+    public DropShotDbContext(DbContextOptions<DropShotDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) => 
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+}
