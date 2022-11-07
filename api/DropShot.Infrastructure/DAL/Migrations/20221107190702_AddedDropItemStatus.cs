7a3f40623edf7491c2e93c87c1c50e5dbbbc3821
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-11-07 20:08:53 +0100

    drop items status

---
+++ api/DropShot.Infrastructure/DAL/Migrations/20221107190702_AddedDropItemStatus.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DropShot.Infrastructure.DAL.Migrations
+{
+    public partial class AddedDropItemStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "DropItems",
+                type: "text",
+                nullable: false,
+                defaultValue: "Available");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "DropItems");
+        }
+    }
+}
