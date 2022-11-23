6d42f4bd24ba8ab605ae5b523f7291be1022e778
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2022-11-23 23:00:27 +0100

    drops socket publisher

---
+++ api/DropShot.Application/Common/IDropsSocketPublisher.cs
@@ -0,0 +1,7 @@
+﻿namespace DropShot.Application.Common;
+
+public interface IDropsSocketPublisher
+{
+    Task DropItemIsReserved(int dropId, int dropItemId);
+    Task DropItemIsReleased(int dropId, int dropItemId);
+}
