bc08db205e818ebcf026a8574fbcc6d864018aae
Konrad Lubera <konradlubera@hotmail.com>
2022-11-26 00:19:07 +0100

    Edited app.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
 import TopBar from './components/Common/TopBar/TopBar';
 import Account from './routes/Account/Account';
@@ -16,32 +16,52 @@ import ErrorPage from './routes/ErrorPage/ErrorPage';
 import Login from './routes/Login/Login';
 import Register from './routes/Register/Register';
 import Root from './routes/Root/Root';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './routes/Auth/PrivateRoute';
+import AdminRoute from './routes/Auth/AdminRoute';
+import AddAdmin from './routes/AdminPanel/UsersManagement/AddAdmin/AddAdmin';
+import AdminList from './routes/AdminPanel/UsersManagement/AdminList/AdminList';
+import { SnackbarProvider } from 'notistack';
 
 const App = () => {
   // useAuth
 
   return (
     <BrowserRouter>
-      <TopBar />
-      <Routes>
-        <Route path="/" element={<Root />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        {/* Authenticated paths */}
-        <Route path="/drops" element={<Drops />} />
-        <Route path="/drops/:dropId" element={<DropDetails />} />
-        <Route path="/admin-panel" element={<AdminPanel />} />
-        <Route path="/admin-panel/users-management" element={<UsersManagement />} />
-        <Route path="/admin-panel/drops-management" element={<DropsManagement />} />
-        <Route path="/admin-panel/drops-management/:dropId" element={<ManageDropPage />} />
-        <Route path="/admin-panel/drops-management/new" element={<AddNewDrop />} />
-        <Route path="/admin-panel/products-management" element={<ProductsManagement />} />
-        <Route path="/admin-panel/products-management/:productId" element={<Product />} />
-        <Route path="/admin-panel/products-management/new" element={<AddNewProduct />} />
-        <Route path="/account" element={<Account />} />
-        {/*  */}
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <AuthProvider>
+        <SnackbarProvider
+          autoHideDuration={1500}
+          maxSnack={2}
+          anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
+        >
+          <TopBar />
+          <Routes>
+            <Route path="/" element={<Root />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* Authenticated paths */}
+            <Route element={<PrivateRoute />}>
+              <Route path="/drops" element={<Drops />} />
+              <Route path="/drops/:dropId" element={<DropDetails />} />
+              <Route path="/account" element={<Account />} />
+            </Route>
+            <Route element={<AdminRoute />}>
+              <Route path="/admin-panel" element={<AdminPanel />} />
+              <Route path="/admin-panel/users-management" element={<UsersManagement />} />
+              <Route path="/admin-panel/users-management/add-admin" element={<AddAdmin />} />
+              <Route path="/admin-panel/users-management/admin-list" element={<AdminList />} />
+              <Route path="/admin-panel/drops-management" element={<DropsManagement />} />
+              <Route path="/admin-panel/drops-management/:dropId" element={<ManageDropPage />} />
+              <Route path="/admin-panel/drops-management/new" element={<AddNewDrop />} />
+              <Route path="/admin-panel/products-management" element={<ProductsManagement />} />
+              <Route path="/admin-panel/products-management/:productId" element={<Product />} />
+              <Route path="/admin-panel/products-management/new" element={<AddNewProduct />} />
+            </Route>
+            {/*  */}
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </SnackbarProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
